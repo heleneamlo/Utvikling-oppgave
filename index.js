86a58b8e3432e58b98392e5a56c1ab4fca40a239
© Jakob 3301™ <danielsen.jakob@gmail.com>
2022-02-01 08:34:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+
+document.addEventListener(`DOMContentLoaded`, ()=>{
+
+})
+
 console.log("lmao");
+const lTetromino = "firstShape"
+const zTetromino = "secondShape"
+const tTetromino = "thirdShape"
+const oTetromino = "fourthShape"
+const iTetromino = "fifthShape"
+let tetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
+
