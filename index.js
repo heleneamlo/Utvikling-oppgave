2c2e47302e42f0c3797784c941d216d244a3545f
© Jakob 3301™ <danielsen.jakob@gmail.com>
2022-02-01 08:45:33 +0100

    js and html

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-const lTetromino = "firstShape"
-const zTetromino = "secondShape"
-const tTetromino = "thirdShape"
-const oTetromino = "fourthShape"
-const iTetromino = "fifthShape"
-let tetrominoes = [lTetromino,zTetromino,tTetromino,oTetromino,iTetromino]
+const lTetromino =[
+    [1, width+1, width2+1, 2],
+    [width, width+1, wdth+2, wdth2+2],
+    [1, width+1, width2+1, width2],
+    [width, width2, width2+1, width2+2]
+];
+const zTetromino = [
+    [0,width,width+1,width2+1],
+    [width+1, width+2,width2,width2+1],
+    [0,width,width+1,width2+1],
+    [width+1, width+2,width2,width2+1]
+  ]
 
-const grid = $("")
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width2+1],
+    [width,width+1,width+2,width2+1],
+    [1,width,width+1,width2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width2+1,width3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width2+1,width3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
