83270862c0bf466f979301c567dddd28f1da3161
© Jakob 3301™ <danielsen.jakob@gmail.com>
2022-02-01 10:18:47 +0100

    first tetrimino works!

--- tetris/index.js
+++ tetris/index.js
@@ -1,37 +1,56 @@
-const GRID = $(".grid")
-console.log(GRID);
-const lTetromino =[
-    [1, width+1, width2+1, 2],
-    [width, width+1, wdth+2, wdth2+2],
-    [1, width+1, width2+1, width2],
-    [width, width2, width2+1, width2+2]
+document.addEventListener('DOMContentLoaded', () => {
+const GRID = $(".grid");
+let squares = Array.from($(".grid div"));
+const SCOREDISPLAY = $("#score");
+const STARTBTN = $("#start-button");
+const width = 10;
+
+const LTETRIMINO =[
+  [1, width+1, width*2+1, 2],
+  [width, width+1, width+2, width*2+2],
+  [1, width+1, width*2+1, width*2],
+  [width, width*2, width*2+1, width*2+2]
 ];
-const zTetromino = [
-    [0,width,width+1,width2+1],
-    [width+1, width+2,width2,width2+1],
-    [0,width,width+1,width2+1],
-    [width+1, width+2,width2,width2+1]
-  ]
+const ZTETRIMINO = [
+  [0,width,width+1,width*2+1],
+  [width+1, width+2,width*2,width*2+1],
+  [0,width,width+1,width*2+1],
+  [width+1, width+2,width*2,width*2+1]
+];
 
-  const tTetromino = [
+  const TTETRIMINO = [
     [1,width,width+1,width+2],
-    [1,width+1,width+2,width2+1],
-    [width,width+1,width+2,width2+1],
-    [1,width,width+1,width2+1]
-  ]
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ];
 
-  const oTetromino = [
+  const OTETRIMINO = [
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1],
     [0,1,width,width+1]
-  ]
+  ];
 
-  const iTetromino = [
-    [1,width+1,width2+1,width3+1],
+  const ITETRIMINO = [
+    [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3],
-    [1,width+1,width2+1,width3+1],
+    [1,width+1,width*2+1,width*3+1],
     [width,width+1,width+2,width+3]
-  ]
+  ];
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino];
+  const TETRIMINOES = [LTETRIMINO, ZTETRIMINO, TTETRIMINO, OTETRIMINO, ITETRIMINO];
+
+  let currentPos = 4;
+  let currentTetr = TETRIMINOES[0][0];
+
+
+  //tegn tetriminioes
+  function draw() {
+    currentTetr.forEach(e => {
+      squares[ currentPos + e ].classList.add("tetromino");
+    });
+  };
+
+  draw();
+})
